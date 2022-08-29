97ee1ab8c2f33c40f296b7d782f19cee5d5ffe2e
Santiago Toyos <ppikachu@gmail.com>
2022-08-29 17:58:11 -0300

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,50 @@
 <script setup lang="ts">
-  import { ref, onMounted, onBeforeMount } from "vue"
-  import { Clock, ACESFilmicToneMapping, Vector2, Vector3, sRGBEncoding, EquirectangularReflectionMapping, UVMapping, MeshStandardMaterial, MeshPhysicalMaterial, TextureLoader, RepeatWrapping, UniformsUtils, ShaderMaterial, Fog, FogExp2 } from "three"
-  import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader"
-  import { SubsurfaceScatteringShader } from "three/examples/jsm/shaders/SubsurfaceScatteringShader.js"
-  import {
-    Camera,
-    GltfModel,
-    PointLight,
-    Renderer,
-    Scene,
-    StandardMaterial,
-    EffectPass,
-  } from 'troisjs'
-  import gsap from 'gsap'
-  import Title from '../src/components/Title.vue'
+import { ref, onMounted, onBeforeMount } from "vue"
+import { Clock, ACESFilmicToneMapping, sRGBEncoding, EquirectangularReflectionMapping, MeshPhysicalMaterial, TextureLoader, Fog } from "three"
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader"
+import {
+  Camera,
+  GltfModel,
+  PointLight,
+  Renderer,
+  Scene,
+} from 'troisjs'
+import Title from '../src/components/Title.vue'
 
-  var light,
-  scene,
-  gltfScene,
-  camera,
-  orbitCtrl,
-  renderer
-
+var gltfScene, renderer, scene
 var clock = new Clock()
 
 const rendererRef = ref()
 const sceneRef = ref()
 const cameraRef = ref()
 const gltfRef = ref()
-const loadedModels = ref(false)
+const loadedModels = ref('opacity-100')
 
-  onBeforeMount(() => {
-    console.clear()
-  })
+onBeforeMount(() => {
+  console.clear()
+})
 
-  onMounted(() => {
-    renderer = rendererRef.value.renderer
-    scene = sceneRef.value.scene
-    renderer.outputEncoding = sRGBEncoding
-    renderer.toneMapping = ACESFilmicToneMapping
-    renderer.toneMappingExposure = 1
-    // turn on the physically correct lighting model
-    renderer.physicallyCorrectLights = true
-  })
+onMounted(() => {
+  renderer = rendererRef.value.renderer
+  scene = sceneRef.value.scene
+  renderer.outputEncoding = sRGBEncoding
+  renderer.toneMapping = ACESFilmicToneMapping
+  renderer.toneMappingExposure = 1
+  // turn on the physically correct lighting model
+  renderer.physicallyCorrectLights = true
+})
 
-  function onReady(gltf) {
+function onReady(gltf) {
   gltfScene = gltf.scene
   const exprimidor = gltfScene.getObjectByName('posta_m')
 
   // car paint
-  const map = new TextureLoader().load( '/exp_baseColor.png' )
-  map.encoding = sRGBEncoding
   const material = new MeshPhysicalMaterial({
     clearcoat: 1,
     clearcoatRoughness: 0.2,
     metalness: 0.4,
     roughness: 0.8,
     color: 0xffffff,
-    map: map,
   })
   // self shadowing
   // exprimidor.receiveShadow = true
@@ -73,19 +60,13 @@ const loadedModels = ref(false)
     scene.environment.mapping = EquirectangularReflectionMapping
     exprimidor.material.envMap = envMap
     exprimidor.material.needsUpdate = true
-    //fadein
-    fadeScene(2)
+    //fade
+    loadedModels.value = 'opacity-0'
   })
 
   exprimidor.material = material
 
   animate()
-}
-
-function fadeScene(time:number) {
-  gsap.to( document.getElementById('fader'), { opacity: 0, duration: time, onComplete: ()=> {
-    loadedModels.value = true
-  } })
 }
 
 function animate() {
@@ -116,8 +97,8 @@ function animate() {
     </Renderer>
     
     <!--fadeScene-->
-    <div id="fader" class="absolute top-0 w-full h-screen flex justify-center items-center bg-gray-100" >
-      <div class="animate-pulse flex flex-col space-y-4 items-center">
+    <div :class="loadedModels" class="transition-opacity duration-200 absolute top-0 w-full h-screen flex justify-center items-center bg-[#ddeeee]" >
+      <div class="animate-pulse flex space-x-4 items-center">
         <svg width="158" height="18" viewBox="0 0 158 18" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" clip-rule="evenodd" d="M4 2H2V16H6V12H10V10H12V4H10V2H6H4ZM8 10V4H6V10H8ZM14 2H18V16H14V2ZM22 4H20V14H22V16H30V14H24V4H30V2H22V4ZM42 2H34V4H32V14H34V16H42V14H44V4H42V2ZM40 4H36V14H40V4ZM48 2H54V4H50V8H52V10H54V14H52V16H46V14H50V10H48V8H46V4H48V2ZM58 16H56V2H58H60H66V4H60V6H64V8H60V14H66V16H60H58ZM72 14H78V16H70V14H68V4H70V2H78V4H72V14ZM88 14H84V4H88V14ZM92 4H90V2H82V4H80V14H82V16H90V14H92V4Z" fill="black"/>
           <path fill-rule="evenodd" clip-rule="evenodd" d="M135 4H134V5H133V6H134V5H135V4ZM124 5H123V6H124V5ZM144 5H145V6H144V5ZM144 5H137V4H144V5ZM145 12V6H146V12H145ZM144 13V12H145V13H144ZM144 13V14H143V13H144ZM153 7H152V8H153V10H152V11H153V10H154V11H155V10H156V8H155V7H154V8H153V7ZM154 8V10H155V8H154ZM128 7H127V10H128V12H130V10H131V7H130V10H128V7ZM151 8H148V9H151V8Z" fill="black"/>
